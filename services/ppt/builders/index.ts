2ded42ae78f1bbd335b071ec026efa2dfed289ae
ChiYu-Huang <iamhandsomeboy1129@gmail.com>
2026-01-15 11:46:38 +0800

    chore(ppt): Set up index file for renderer exports

--- services/ppt/builders/index.ts
+++ services/ppt/builders/index.ts
@@ -1,2 +1,13 @@
-// Export all renderers here
+/**
+ * Export all block renderers.
+ * The RendererRegistry will automatically pick these up via 'Object.values'.
+ */
+
+// Core Renderers (To be implemented in Phase 3)
 // export * from './Heading1Renderer';
+// export * from './Heading2Renderer';
+// export * from './ParagraphRenderer';
+// export * from './ListRenderer';
+// export * from './ImageRenderer';
+// export * from './CodeBlockRenderer';
+// export * from './ChatCustomRenderer';
